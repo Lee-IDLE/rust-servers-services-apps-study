c164f397b597221df551297456c9b69175ccb906
Lee-IDLE <nidus0526@gmail.com>
2024-12-09 20:55:52 +0900

    p.52까지 함

--- chapter2/scenario1/_http/src/httpresponse.rs
+++ chapter2/scenario1/_http/src/httpresponse.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::io::{Result, Write};
 #[derive(Debug, PartialEq, Clone)]
-
+// cargo test -p _http --lib
 pub struct HttpResponse<'a> {
     version: &'a str,
     status_code: &'a str,
@@ -157,5 +157,22 @@ mod tests {
         assert_eq!(response_actual, response_expected);
     }
 
-    
+    #[test]
+    fn test_http_response_creation() {
+        let response_expected = HttpResponse {
+            version: "HTTP/1.1",
+            status_code: "404",
+            status_text: "Not Found",
+            headers: {
+                let mut h = HashMap::new();
+                h.insert("Content-Type", "text/html");
+                Some(h)
+            },
+            body: Some("Item was shipped on 21st Dec 2020".into()),
+        };
+        let http_string: String = response_expected.into();
+        let response_actual = "HTTP/1.1 404 Not Found\r\nContent-Type:text/html\r\nConte-Type:text/html
+        \r\nContent-Length: 33\r\n\r\nItem was shipped on 21st Dec 2020";
+        assert_eq!(http_string, response_actual);
+    }
 }
--- chapter2/scenario1/_httpserver/src/main.rs
+++ chapter2/scenario1/_httpserver/src/main.rs
@@ -1,3 +1,12 @@
+mod handler;
+mod server;
+mod router;
+
+use server::Server;
+
 fn main() {
-    println!("Hello, world!");
+    // 서버를 시작한다.
+    let server = Server::new("localhost:3000");
+    // 서버를 실행한다.
+    server.run();
 }
