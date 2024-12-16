6ce07f74274d71ce4dc4a8ff0abe8a3a03802698
Lee-IDLE <nidus0526@gmail.com>
2024-12-16 22:27:33 +0900

    p.86까지 함
    테스트 코드는 됐는데 데이터 전송 테스트 실패함.
    p.87 '1단계 라우트 구성 정의하기' 할 차례

--- chapter3/ezytutors/tutor-nodb/src/routes.rs
+++ chapter3/ezytutors/tutor-nodb/src/routes.rs
@@ -1,7 +1,11 @@
-use super::handlers::*;
+use super::handler::*;
 use actix_web::web;
 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
 }
 
+pub fn course_routes(cfg: &mut web::ServiceConfig) {
+    cfg
+    .service(web::scope("/courses").route("/", web::post().to(new_course)));
+}
--- chapter3/ezytutors/tutor-nodb/src/state.rs
+++ chapter3/ezytutors/tutor-nodb/src/state.rs
@@ -1,9 +1,12 @@
 use std::sync::Mutex;
+use super::model::Course;
 
 pub struct AppState {
     // 공유된 이뮤터블 상태
     pub health_check_response: String, 
     // 공유된 뮤터블 상태
     pub visit_count: Mutex<u32>,
+    // 강의들은 Vec 컬렉션으로 애플리케이션 상태에 저장, Mutex로 보호
+    pub courses: Mutex<Vec<Course>>,
 }
 
