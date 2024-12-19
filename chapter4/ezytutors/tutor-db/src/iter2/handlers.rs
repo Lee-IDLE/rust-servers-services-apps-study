c57ff9d619826097c8ae0dbaefa9425f576ff8d3
Lee-IDLE <nidus0526@gmail.com>
2024-12-19 23:09:47 +0900

    p.112 까지 함
    코드는 다 됐는데 빌드 도중 OpenSSL에서 멈춤.

--- chapter4/ezytutors/tutor-db/src/iter2/handlers.rs
+++ chapter4/ezytutors/tutor-db/src/iter2/handlers.rs
@@ -34,3 +34,68 @@ pub async fn post_new_course(
 ) -> HttpResponse {
     HttpResponse::Ok().json("Success");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::StatusCode;
+    use chrono::NaiveDate;
+    use dotenv::dotenv;
+    use sqlx::postgres::PgPool;
+    use std::env;
+    use std::sync::Mutex;
+
+    #[actix_rt::test]
+    async fn get_all_courses_success() {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect(
+            "DATABASE_URL is not set in .env file");
+        let pool: PgPool = PgPool::connect(&database_url).await.unwrap();
+        let app_state = web::Data::new(AppState{
+            health_check_handler: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: pool
+        });
+        let tutor_id: web::Path<(usize,)> = web::Path::from((1,));
+        let resp = get_course_for_tutor(app_state, tutor_id).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn get_course_detail_test() {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect(
+            "DATABASE_URL is not set in .env file");
+        let pool: PgPool = PgPool::connect(&database_url).await.unwrap();
+        let app_state = web::Data::new(AppState{
+            health_check_handler: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: pool
+        });
+        let params: web::Path<(i32, i32)> = web::Path::from((1, 2));
+        let resp = get_course_details(app_state, params).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn post_course_success() {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect(
+            "DATABASE_URL is not set in .env file");
+        let pool: PgPool = PgPool::connect(&database_url).await.unwrap();
+        let app_state = web::Data::new(AppState{
+            health_check_handler: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: pool
+        });
+        let new_course_msg = Course {
+            course_id: Some(1),
+            tutor_id: 1,
+            course_name: "This is the next course".into(),
+            posted_time: Some(naiveDate::from_ymd(2020, 9, 17)).and_hms(14, 01, 11)
+        };
+        let course_param = web::Json(new_course_msg);
+        let resp = post_new_course(course_param, app_state).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+}
