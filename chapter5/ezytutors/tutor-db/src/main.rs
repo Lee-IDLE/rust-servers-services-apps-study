6ddcdaef1715351d7e507564063516fa983de890
Lee-IDLE <94844529+Lee-IDLE@users.noreply.github.com>
2024-12-24 13:53:36 +0900

    CHAPTER 5를 위한 기본 코드 준비 완료
    p.136부터 시작하면 됨

--- chapter5/ezytutors/tutor-db/src/main.rs
+++ chapter5/ezytutors/tutor-db/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{error::Error, web, App, HttpResponse, HttpServer};
+
+async fn hello() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().body("Hello there!"))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/hello", web::get().to(hello)))
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
