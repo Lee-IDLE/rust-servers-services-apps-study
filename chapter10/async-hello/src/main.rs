321adb35e4105de70357f8ea06c0b7cca600a9a6
Lee-IDLE <nidus0526@gmail.com>
2025-01-15 23:35:37 +0900

    주석 추가
    ㅇㅇ

--- chapter10/async-hello/src/main.rs
+++ chapter10/async-hello/src/main.rs
@@ -11,7 +11,11 @@ impl Future for ReadFileFuture {
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         println!("Tokio! Stop polling me");
-        Poll::Pending
+        // Waker 인스턴스에 대한 wake_by_ref() 함수가 호출된다.
+        // 이는 차례로 Tokio 런타임에게 해당 비동기 태스크를 실행을 위해
+        // 다시 스케줄링 할 준비가 되었음을 알린다.
+        cx.waker().wake_by_ref(); // 깨어나서 실행할 준비가 되었으니 다시 깨워주세요
+        Poll::Pending // 대기 모드로 전환
     }
 }
 
@@ -20,9 +24,15 @@ async fn main() {
     println!("Helo before reading file!");
 
     let h1 = tokio::spawn(async {
-        let file_contents = read_from_file2().await;
+        let future1 = ReadFileFuture {};
+        future1.await
+    });
+
+    let h2 = tokio::spawn(async {
+        let file2_contents = read_from_file2().await;
         println!("{:?}", file2_contents);
     });
+
     let _ = tokio::join!(h1, h2);
 }
 
