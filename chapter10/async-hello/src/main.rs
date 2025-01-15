32af745e94a4da3822b9568dbf32bd30c6986d1f
Lee-IDLE <94844529+Lee-IDLE@users.noreply.github.com>
2025-01-15 16:54:45 +0900

    CHAPTER 10 진입
    p.292까지 함

---
+++ chapter10/async-hello/src/main.rs
@@ -0,0 +1,36 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::thread::sleep;
+use std::time::Duration;
+
+struct ReadFileFuture {}
+
+impl Future for ReadFileFuture {
+    type Output = String;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        println!("Tokio! Stop polling me");
+        Poll::Pending
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    println!("Helo before reading file!");
+
+    let h1 = tokio::spawn(async {
+        let file_contents = read_from_file2().await;
+        println!("{:?}", file2_contents);
+    });
+    let _ = tokio::join!(h1, h2);
+}
+
+// 파일 읽기를 시뮬레이션 하는 함수
+fn read_from_file2() -> impl Future<Output = String> {
+    async {
+        sleep(Duration::new(2, 0));
+        println!("{:?}", "Processing file 2");
+        String::from("Hello, there from file 2")
+    }
+}
